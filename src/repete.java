34eee6108af82cf231a2ba1e9d3fb4b97fb80975
Vinicius Sá <viniciussouza33@gmail.com>
2026-05-28 19:51:10 -0500

    feat: repete

---
+++ src/repete.java
@@ -0,0 +1,10 @@
+public class repete {
+
+    public static void main(String[] args) {
+        int contador = 0;
+        while (contador < 3) {
+            System.out.println("Olá,!");
+            contador++;
+        }
+    }
+}
